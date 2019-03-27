be35324bcda51bf66ae6bb1fb4580ce7593794a6
tengfei8771 <532050072@qq.com>
2019-03-27 14:36:11 +0800

    1

---
+++ Repository/BaseRepository/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.BaseRepository
+{
+    public interface IBaseRepository<T> where T:class
+    {
+        int Insert(T entity);
+        int Delete(T entity);
+        int edit(T entity);
+    }
+}
