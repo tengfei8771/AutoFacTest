bf1e225f337922c1cf2f005e147fda2c64a047a4
tengfei8771 <532050072@qq.com>
2019-03-29 15:57:25 +0800

    1

--- Repository/BaseRepository/BaseRepository.cs
+++ Repository/BaseRepository/BaseRepository.cs
@@ -1,10 +1,59 @@
-﻿using System;
+﻿using Entity.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Repository.BaseRepository
 {
-    public class BaseRepository
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
+
+        private AppDBContext _appDBContext;
+        public BaseRepository(AppDBContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+        public int Delete(T entity)
+        {
+            _appDBContext.Remove(entity);
+            return _appDBContext.SaveChanges();
+        }
+
+        public int Edit(T entity)
+        {
+            _appDBContext.Update(entity);
+            return _appDBContext.SaveChanges();
+        }
+
+        public T GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetListByPageWhere(Expression<Func<T, bool>> predicate, int page, int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetListWhere(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> QueryJoin(Expression<Func<T, bool>> predicate, string[] tableNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> QueryJoin(Expression<Func<T, bool>> predicate, string[] tableNames, int page, int limit)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Repository/BaseRepository/IBaseRepository.cs
+++ Repository/BaseRepository/IBaseRepository.cs
@@ -1,14 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Repository.BaseRepository
 {
     public interface IBaseRepository<T> where T:class
     {
+        /// <summary>
+        /// 增加
+        /// </summary>
+        /// <param name="entity">实体类</param>
+        /// <returns>受影响的行数</returns>
         int Insert(T entity);
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="entity">实体类</param>
+        /// <returns>受影响的行数</returns>
         int Delete(T entity);
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="entity">实体类</param>
+        /// <returns>受影响的行数</returns>
         int Edit(T entity);
 
+        /// <summary>
+        /// 根据ID获取实体类
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <returns>实体类</returns>
+        T GetById(string id);
+        /// <summary>
+        /// lambda表达式查询
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns>实体类list</returns>
+        List<T> GetListWhere(Expression<Func<T, bool>> predicate);
+        /// <summary>
+        /// 多表联查
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="tableNames"></param>
+        /// <returns>实体类list</returns>
+        List<T> QueryJoin(Expression<Func<T, bool>> predicate, string[] tableNames);
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <param name="predicate">表达式</param>
+        /// <param name="page">页数</param>
+        /// <param name="limit">每页最大条数</param>
+        /// <returns></returns>
+        List<T> GetListByPageWhere(Expression<Func<T, bool>> predicate,int page,int limit);
+
+        /// <summary>
+        /// 多表联查分页方法
+        /// </summary>
+        /// <param name="predicate">表达式</param>
+        /// <param name="tableNames">表名</param>
+        /// <param name="page">页数</param>
+        /// <param name="limit">每页条数</param>
+        /// <returns>实体类list</returns>
+        List<T> QueryJoin(Expression<Func<T, bool>> predicate, string[] tableNames,int page,int limit);
     }
 }
