7fbc4c40162e210d3b8b4101c69e4a386b50e29a
tengfei8771 <532050072@qq.com>
2019-07-04 15:14:22 +0800

    1.修改

---
+++ WeChatPay/WxApi/Native.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeChatPay.WxApi
+{
+    public class Native
+    {
+        public void UniteOrder()
+        {
+            WxPayData wd = new WxPayData();
+            wd.SetValue("appid", WxPayConfig.appid);
+            wd.SetValue("mch_id", WxPayConfig.mchid);
+            wd.SetValue("device_info", "Web");
+            wd.SetValue("nonce_str", WxUntil.GetRandomStr());
+            wd.SetValue("sign_type", "MD5");
+            wd.SetValue("fee_type", "CNY");
+        }
+    }
+}
